849b1babc17b1c8e9ba9d0326dbfe4ae812b1551
RobotMan2412 <julian@scheffers.net>
2025-07-15 23:24:03 +0200

    Add some more effects

--- main/effects.c
+++ main/effects.c
@@ -14,10 +14,10 @@ float brightness = 1;
 // A uint8_t red, green, blue tuple.
 typedef struct {
     uint8_t r, g, b;
-} rbg_t;
+} rgb_t;
 
 // Convert float HSV into uint8_t RGB.
-static rbg_t f_hsv_to_rgb(float h, float s, float v) {
+static rgb_t f_hsv_to_rgb(float h, float s, float v) {
     h = fmodf(h, 1);
     float r, g, b;
 
@@ -39,7 +39,16 @@ static rbg_t f_hsv_to_rgb(float h, float s, float v) {
     }
     // clang-format on
 
-    rbg_t rgb;
+    rgb_t rgb;
+    rgb.r = (uint8_t)(r * 255.0f);
+    rgb.g = (uint8_t)(g * 255.0f);
+    rgb.b = (uint8_t)(b * 255.0f);
+    return rgb;
+}
+
+// Convert float RGB into uint8_t RGB.
+static rgb_t f_rgb(float r, float g, float b) {
+    rgb_t rgb;
     rgb.r = (uint8_t)(r * 255.0f);
     rgb.g = (uint8_t)(g * 255.0f);
     rgb.b = (uint8_t)(b * 255.0f);
@@ -50,7 +59,7 @@ static rbg_t f_hsv_to_rgb(float h, float s, float v) {
 static uint8_t led_data[3 * LED_COUNT];
 
 // Set the LED data for a particular LED.
-static inline void set_led(size_t index, rbg_t col) {
+static inline void set_led(size_t index, rgb_t col) {
     led_data[index * 3 + 0] = col.r * brightness;
     led_data[index * 3 + 1] = col.g * brightness;
     led_data[index * 3 + 2] = col.b * brightness;
@@ -61,17 +70,40 @@ static void update_leds() {
     bsp_led_write(led_data, sizeof(led_data));
 }
 
-// A simple hue shifting effect.
-static void effect_hueshift(float coeff) {
+// A simple hue spectrum effect.
+static void effect_hue_spectrum(float coeff) {
     for (size_t i = 0; i < LED_COUNT; i++) {
         set_led(i, f_hsv_to_rgb(coeff + i / (float)LED_COUNT, 1, 1));
     }
     update_leds();
 }
 
+// A uniform hue shift.
+static void effect_hue_single(float coeff) {
+    rgb_t col = f_hsv_to_rgb(coeff, 1, 1);
+    for (size_t i = 0; i < LED_COUNT; i++) {
+        set_led(i, col);
+    }
+    update_leds();
+}
+
+// A knight rider like effect.
+static void effect_knight_rider(float coeff) {
+    coeff     = fmodf(coeff, 1);
+    float pos = coeff < 0.5 ? coeff * 2 : 2 - coeff * 2;
+    for (size_t i = 0; i < LED_COUNT; i++) {
+        float dist = pos - i / (float)(LED_COUNT - 1);
+        float a    = fmaxf(0, 1 - 4.5 * dist * dist);
+        set_led(i, f_rgb(0, a * 0.8, a));
+    }
+    update_leds();
+}
+
 // Table of all effects.
 effect_t const effects[] = {
-    effect_hueshift,
+    effect_hue_spectrum,
+    effect_hue_single,
+    effect_knight_rider,
 };
 
 // Number of effects.
